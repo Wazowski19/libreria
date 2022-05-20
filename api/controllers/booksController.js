293a23a9d136f175fa9fa577faa02146765abf2e
Miguel Ángel García Martínez <ma.garmar98@gmail.com>
2022-05-19 20:14:58 -0500

    "feat: book consult added"

--- api/controllers/booksController.js
+++ api/controllers/booksController.js
@@ -3,10 +3,10 @@ import connection from '../utils/database.js';
 const getBooks = async (req, res) => {
   try {
     const books = await connection.select('*').from('books');
-    //const category = await connection.select('*').from('category').join('books', 'books.category_id', 'category.books_id').where('category.category_id', '=', 1)
+    const category = await connection.select('category_name').from('category').join('books_category', 'books_category.category_id', 'category.category_id').where('book_id', '=', 1)
     return res.json({
       books,
-      //category
+      category
     });
   } catch (err) {
     console.error(err);
@@ -16,4 +16,22 @@ const getBooks = async (req, res) => {
   }
 };
 
-export { getBooks };
+/* const insertBooks = async (req, res) => {
+  const { book } = req.body;
+
+  try {
+    const newUser = await connection('books').insert(book).returning('*');
+    // const newUser = await connection.table('film').insert(user);
+    return res.json({
+      msg: 'Usuario registrado correctamente 😂',
+      user: newUser,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      msg: 'Error al registrar nuevo usuario',
+    });
+  }
+}; */
+
+export { getBooks, /* insertBooks */ };
--- api/routes/booksRoute.js
+++ api/routes/booksRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getBooks } from '../controllers/booksController.js';
+import { getBooks, /* insertBooks */ } from '../controllers/booksController.js';
 
 const router = express.Router();
 
 router.get('/books', getBooks);
+/* router.post('/books', insertBooks) */
 
 
 export default router;
